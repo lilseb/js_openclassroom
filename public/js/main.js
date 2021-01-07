4d429f0142216ccc185daec50bccee4dc9748432
Sébastien <seb.lasante@hotmail.com>
2021-01-07 19:14:05 +0100

    debut révision

---
+++ public/js/main.js
@@ -0,0 +1,29 @@
+// let numberOfCats = 2
+// let numberOfDogs = 4
+
+
+
+
+// Déclarez des variables et modifiez leurs valeurs
+
+// let numberOfSeasons = 6;
+// let numberOfEpisodes = 12;
+// let totalepisodes = numberOfSeasons * numberOfEpisodes; 
+// console.log(totalepisodes);
+// // total de 72 épisodes
+
+
+// let episodeTime = 45;
+// let commercialTime = 5;
+// // let totalShowTime = (episodeTime * totalepisodes) + (commercialTime * totalepisodes)
+// // console.log(totalShowTime);
+// // OR
+// let totalShowTime = (episodeTime + commercialTime) * numberOfSeasons * numberOfEpisodes;
+// console.log(totalShowTime);
+
+
+
+
+// Enregistrez vos données avec des types de données
+
+
