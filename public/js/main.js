3380de3a1eb92878f127f1f9af35a57497d3a666
Sébastien <seb.lasante@hotmail.com>
2021-01-12 18:20:20 +0100

    tableau

--- public/js/main.js
+++ public/js/main.js
@@ -26,4 +26,49 @@
 
 // Enregistrez vos données avec des types de données
 
+// let episodeTitle = ("Nuit blanche")
+// let episodeDuration = (50)
+// let hasBeenWatched = true
 
+
+
+
+// let guests = ["Will Alexander", "Sarah Kate", "Audrey Simon"];
+// console.log(guests);
+// // Le comptage d'éléments
+// console.log(guests.length);
+
+// let howManyGuests = guests.length; // 3
+// console.log(howManyGuests);
+
+
+
+// // L'ajout et la suppression d'éléments
+
+// // ajoute "Seb" à la fin de notre tableau guests
+// guests.push("Seb")
+// console.log(guests);
+
+// // "Seb" est ajouté au début du tableau guests
+// guests.unshift("Seb")
+// console.log(guests);
+
+// // supprimer le dernier élément du tableau
+// guests.pop()
+// console.log(guests);
+
+
+
+
+
+
+
+// let episodes = [];
+// episodes.push("firstEpisode", "secondEpisode", "thirdEpisode", "thirdEpisode");
+// console.log(episodes);
+
+// episodes.pop()
+// console.log(episodes);
+
+// let numberOfEpisodes = episodes.length
+// console.log(numberOfEpisodes);
